f65b20feb4f7e90fb7fbcce2521e62942fb04360
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2023-07-04 00:29:19 +0200

    Added validation of download URL

--- download.sh
+++ download.sh
@@ -2,13 +2,18 @@
 
 if [ "$#" -ne 2 ]; then
   echo "Usage: $0 <URL> <DESTINATION>"
-  echo "   eg: $0  https://civitai.com/api/download/models/15236 /workspace/stable-diffusion-webui/models/Stable-diffusion"
+  echo "   eg: $0 https://civitai.com/api/download/models/15236 /workspace/stable-diffusion-webui/models/Stable-diffusion"
   exit 1
 fi
 
 URL=${1}
 DESTINATION=${2}
 USER_AGENT_STRING="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+
+if ! echo "${URL}" | grep -q "api"; then
+  echo "ERROR: Incorrect URL provided, you must provide the Download link from CivitAI, not the link to the model page."
+  exit 1
+fi
 
 echo "Downloading model from ${URL}, please wait..."
 
