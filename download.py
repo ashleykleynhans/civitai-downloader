aa5b2fddb2fec5f63949ade20ab7f91c80f12033
Lorenzo Garuti <garuti.lorenzo@gmail.com>
2025-08-26 14:45:06 +0200

    Update download.py
    Get token from env_vars
    Pass only model ids and not the full url
    Unzip downloaded models if the file name ends with .zip

--- download.py
+++ download.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
-import os.path
+import os
 import sys
 import argparse
 import time
 import urllib.request
+import zipfile
 from pathlib import Path
 from urllib.parse import urlparse, parse_qs, unquote
-
 
 CHUNK_SIZE = 1638400
 TOKEN_FILE = Path.home() / '.civitai' / 'config'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-
+DEFAULT_ENV_NAME = os.getenv("CIVITAI_TOKEN_NAME", "CIVITAI_TOKEN")
+CIVITAI_BASE_URL = 'https://civitai.com/api/download/models'
 
 def get_args():
     parser = argparse.ArgumentParser(
@@ -19,9 +20,9 @@ def get_args():
     )
 
     parser.add_argument(
-        'url',
+        'model_id',
         type=str,
-        help='CivitAI Download URL, eg: https://civitai.com/api/download/models/46846'
+        help='CivitAI Download Model ID, eg: 46846'
     )
 
     parser.add_argument(
@@ -32,8 +33,10 @@ def get_args():
 
     return parser.parse_args()
 
-
 def get_token():
+    token = os.getenv(DEFAULT_ENV_NAME, None)
+    if token:
+        return token
     try:
         with open(TOKEN_FILE, 'r') as file:
             token = file.read().strip()
@@ -41,21 +44,18 @@ def get_token():
     except Exception as e:
         return None
 
-
 def store_token(token: str):
     TOKEN_FILE.parent.mkdir(parents=True, exist_ok=True)
 
     with open(TOKEN_FILE, 'w') as file:
         file.write(token)
 
-
 def prompt_for_civitai_token():
     token = input('Please enter your CivitAI API token: ')
     store_token(token)
     return token
 
-
-def download_file(url: str, output_path: str, token: str):
+def download_file(model_id: str, output_path: str, token: str):
     headers = {
         'Authorization': f'Bearer {token}',
         'User-Agent': USER_AGENT,
@@ -67,6 +67,7 @@ def download_file(url: str, output_path: str, token: str):
             return response
         https_response = http_response
 
+    url = f'{CIVITAI_BASE_URL}/{model_id}'
     request = urllib.request.Request(url, headers=headers)
     opener = urllib.request.build_opener(NoRedirection)
     response = opener.open(request)
@@ -150,6 +151,13 @@ def download_file(url: str, output_path: str, token: str):
     print(f'Download completed. File saved as: {filename}')
     print(f'Downloaded in {time_str}')
 
+    if output_file.endswith('.zip'):
+        print('Note: The downloaded file is a ZIP archive.')
+        try:
+            with zipfile.ZipFile(output_file, 'r') as zip_ref:
+                zip_ref.extractall(os.path.dirname(output_file))
+        except Exception as e:
+            print(f'ERROR: Failed to unzip the file. {e}')
 
 def main():
     args = get_args()
@@ -159,10 +167,9 @@ def main():
         token = prompt_for_civitai_token()
 
     try:
-        download_file(args.url, args.output_path, token)
+        download_file(args.model_id, args.output_path, token)
     except Exception as e:
         print(f'ERROR: {e}')
-
 
 if __name__ == '__main__':
     main()
